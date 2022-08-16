546b0f89ae109055b8aba77835559fbbd55a0535
Toby <0xdegn@gmail.com>
2022-08-17 00:04:33 +0100

    Added a messages subfolder. Import a message module and use the module

---
+++ Unit 1/01 - Messages/messages.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.messages = [
+    "A change of environment can be a good thing",
+    "You will make it!",
+    "Just run with the code"
+];
+
+
---
+++ Unit 1/01 - Messages/positiveMessages.js
@@ -0,0 +1,8 @@
+// "use strict";
+let messages = [
+    "A change of environment can be a good thing",
+    "You will make it!",
+    "Just run with the code"
+];
+
+messages.forEach(value => console.log(value));
